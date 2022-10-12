b1e27d83eaed1629c0f1cf8bd547e6def09ce21a
Artem Grabovski <art.grabovski@gmail.com>
2022-10-12 13:40:01 +0300

    Initial Commit

---
+++ HomeWork14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork14
+//
+//  Created by Darya Grabowskaya on 12.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
